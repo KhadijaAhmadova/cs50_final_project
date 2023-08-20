3ffbce2395ef20b08653b74735220aaec552de68
KhadijaAhmadova <xadija.ahmedova@gmail.com>
2023-08-21 00:33:39 +0400

    login logout index routes in the app

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 from flask import Flask, request, render_template, redirect
 from helpers import apology
 import sqlite3
-from werkzeug import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL)")
-
-cursor.execute("CREATE TABLE tasks(id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL, task_name TEXT NOT NULL, due_date TEXT, category TEXT, FOREIGN KEY (user_id) REFERENCES users (id))")
-
-connection.commit()
-
-
 app = Flask(__name__)
+
+
+session = []
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    tasks = cursor.execute("SELECT * FROM tasks WHERE user_id = ?", session["id"])
+    return render_template("index.html", tasks=tasks)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    session.clear()
+
     if request.method == "GET":
         return render_template("register.html") 
     else:
@@ -46,12 +45,33 @@ def register():
         cursor.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
         connection.commit()
 
+        session["id"] = rows[0]["id"]
         return redirect("/")
     
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    
+    session.clear()
+
     if request.method == "GET":
         return render_template("login.html")
+    
     else:
+        if not request.form.get("username"):
+            return apology("must provide username")
+        if not request.form.get("password"):
+            return apology("must provide password")
+        
+        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password")
+        
+        session["id"] = rows[0]["id"]
         return redirect("/")
+    
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
